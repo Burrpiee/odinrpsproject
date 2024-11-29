1adc0d1888106c935f2cd66e98d9fa0034749f2f
Burrpie <135572400+Burrpiee@users.noreply.github.com>
2024-11-29 21:15:33 +0800

    Add logic to determine game winner.
    Game winner is decided by whoever has a score of 5 first.

--- script.js
+++ script.js
@@ -14,29 +14,37 @@ function getComputerChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+const container = document.querySelector("#container");
+const scoreboard= document.createElement("div");
+
 function playRound(humanChoice, computerChoice){
 
     switch (true){
         case (humanChoice == computerChoice):
             console.log("It's a tie.");
+            scoreboard.textContent = `Your score: ${humanScore}  Computer score: ${computerScore}`;
+            container.appendChild(scoreboard);
             break;
         
         case (humanChoice == "scissors" && computerChoice == "paper"):
         case (humanChoice == "paper" && computerChoice == "rock"):
         case (humanChoice == "rock" && computerChoice == "scissors"):
-            console.log("You won!");
             humanScore++;
+            console.log("You won!")
+            scoreboard.textContent = `You won! Your score: ${humanScore}  Computer score: ${computerScore}`;
+            container.appendChild(scoreboard);
+            gameWinner();
             break;
 
         case (computerChoice == "scissors" && humanChoice == "paper"):
         case (computerChoice == "paper" && humanChoice == "rock"):
         case (computerChoice == "rock" && humanChoice == "scissors"):
-            console.log("You lost!");
             computerScore++;
+            console.log ("You lost.")
+            scoreboard.textContent = `You lost. Your score: ${humanScore}  Computer score: ${computerScore}`;
+            container.appendChild(scoreboard);
+            gameWinner();
             break; 
-        default:
-            break;
-
     }
 }
 
@@ -53,3 +61,17 @@ paper.addEventListener("click", () => {
 scissors.addEventListener("click", () => {
     playRound("scissors", getComputerChoice())
 });
+
+const results = document.createElement("div");
+
+function gameWinner(){
+if (computerScore == 5){
+    results.textContent = "You have lost the game.";
+    container.appendChild(results);
+}
+else if (humanScore == 5){
+    results.textContent = "You have won the game!";
+    container.appendChild(results);
+}
+}
+
